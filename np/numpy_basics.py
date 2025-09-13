509c53fe93519dd9b66127c11c51ef26e8f980d8
inhadeepblue <deepblue@inha.ac.kr>
2025-09-13 16:28:19 +0900

    array property

--- np/numpy_basics.py
+++ np/numpy_basics.py
@@ -1,21 +1,20 @@
 import numpy as np
 
-# list1 = [1, 2, 3.7, 4, '5']
-list1 = [1, 2, 7, 4, 5]
-array1 = np.array(list1)
-array2 = np.array([[1, 2, 3],[4, 5, 6]])
-
-zeros = np.zeros((3, 4))
-ones = np.ones((2, 3))
-id = np.eye(3)
-range_array = np.arange(0, 10, 2)
-linspace = np.linspace(0, 1, 5)
-
-print(range_array)
-print(linspace)
-# print(list1)
-# print(array1)
-# print(array2)
-# print(zeros)
-# print(ones)
-# print(id)
+array = np.array(
+    [
+        [
+            [3, 2, 1],
+            [4, 5, 6]
+        ],
+        [
+            [9, 2, 31],
+            [4, 15, -6]
+        ]
+    ]
+)
+print(array[1, 0, 2])
+print(array.shape)
+print(array.ndim)
+print(array.size)
+print(array.dtype)
+print(array)
