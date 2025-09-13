8f201a7b86314c5e4bbdf200e46d2cb2e6aef7e0
inhadeepblue <deepblue@inha.ac.kr>
2025-09-13 16:47:24 +0900

    array if, filter

--- np/numpy_basics.py
+++ np/numpy_basics.py
@@ -1,17 +1,10 @@
 import numpy as np
 
-# np.random.seed(50)
+a = np.arange(1, 6)
 
-x = np.random.random(5)
-print(x)
-y = np.random.randint(1, 7, 5)
-print(y)
-z = np.random.normal(50, 10, 5)
-print(z)
-# q = np.arange(1, 6)
-# choice = np.random.choice(q, 3)
-# print(choice)
+mask = a > 2
+print(a[mask])
 
-q = np.array(["가위", "바위", "보"])
-choice = np.random.choice(q, 2)
-print(choice)
+# result = np.where(a > 3, a, a**2)
+result = np.where(a > 3, a, 0)
+print(result)
