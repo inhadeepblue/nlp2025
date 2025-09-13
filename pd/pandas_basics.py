d95ef2f3e3dc73be171415f4ed25a834b1a59f3e
inhadeepblue <deepblue@inha.ac.kr>
2025-09-13 17:22:52 +0900

    create dataframe

---
+++ pd/pandas_basics.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+df1 = pd.DataFrame(
+    {"id": [100, 102, 107],
+     "math" : [99, 100, 97],
+     "english" : [95, 92, 94]
+     }, index=[1, 2, 3]
+)
+print(df1)
+df2 = pd.DataFrame(
+    [
+        [100, 99, 95],
+        [102, 100, 92],
+        [107, 97, 94],
+    ], columns=["id", "math", "english"], index=[1, 2, 3]
+)
+print(df2)
