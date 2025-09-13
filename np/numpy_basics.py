314c03c4cd19e63a6ea9286a632240599dcfde46
inhadeepblue <deepblue@inha.ac.kr>
2025-09-13 16:23:51 +0900

    create array

---
+++ np/numpy_basics.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# list1 = [1, 2, 3.7, 4, '5']
+list1 = [1, 2, 7, 4, 5]
+array1 = np.array(list1)
+array2 = np.array([[1, 2, 3],[4, 5, 6]])
+
+zeros = np.zeros((3, 4))
+ones = np.ones((2, 3))
+id = np.eye(3)
+range_array = np.arange(0, 10, 2)
+linspace = np.linspace(0, 1, 5)
+
+print(range_array)
+print(linspace)
+# print(list1)
+# print(array1)
+# print(array2)
+# print(zeros)
+# print(ones)
+# print(id)
