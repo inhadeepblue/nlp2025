b759d65e6f04e5044a7123a44faf8586407d3928
inhadeepblue <deepblue@inha.ac.kr>
2025-09-13 17:31:37 +0900

    method chaining

--- pd/pandas_basics.py
+++ pd/pandas_basics.py
@@ -1,12 +1,13 @@
+import numpy as np
 import pandas as pd
 
-df1 = pd.DataFrame(
-    {"id": [100, 102, 107],
-     "math" : [99, 100, 97],
-     "english" : [95, 92, 94]
-     }, index=[1, 2, 3]
-)
-print(df1)
+# df1 = pd.DataFrame(
+#     {"id": [100, 102, 107],
+#      "math" : [99, 100, 97],
+#      "english" : [95, 92, 94]
+#      }, index=[1, 2, 3]
+# )
+# print(df1)
 df2 = pd.DataFrame(
     [
         [100, 99, 95],
@@ -15,3 +16,7 @@ df2 = pd.DataFrame(
     ], columns=["id", "math", "english"], index=[1, 2, 3]
 )
 print(df2)
+# df3 = pd.melt(df2)
+# df3 = pd.melt(df2).rename(columns={'variable':'var', 'value':'val'})
+df3 = pd.melt(df2).rename(columns={'variable':'var', 'value':'val'}).query('val > 95')
+print(df3)
